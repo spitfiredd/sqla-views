4d869a9fc98da37e896761df242bae5bd484cd35
Daniel Donovan <spitfiredd@gmail.com>
2019-02-23 07:05:49 -0500

    add metadata info

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup(
     name='sqla-views',
     version='0.1',
     packages=find_packages(),
+    url='https://github.com/spitfiredd/sqla-views.git',
+    author='Daniel Donovan',
+    author_email='spitfiredd@gmail.com',
     include_package_data=True,
     install_requires=[
         'sqlalchemy',
